74ef4e23d5bc1dac8398b720f709c8e5a9069be9
Brian Babu <briansmkenya@gmail.com>
2023-04-04 14:41:53 +0300

    initial commit

---
+++ index.js
@@ -0,0 +1,7 @@
+fetch("http://localhost:3000/apartments")
+.then(response => response.json())
+
+.then(data => {
+    // Content container
+    let container = document.getElementById('container');
+})
